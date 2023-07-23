408106c4a8a130946613462681924265c962aac2
ymatsunaga <ymatsunaga@mail.saitama-u.ac.jp>
2023-07-23 21:16:26 +0900

    globalParameter -> parameterPerAtom

--- t4_lysozyme/2_equilibration.py
+++ t4_lysozyme/2_equilibration.py
@@ -42,13 +42,15 @@ integrator.setConstraintTolerance(constraintTolerance)
 # Positional restraints
 restraint = mm.openmm.CustomExternalForce('k*periodicdistance(x, y, z, x0, y0, z0)^2')
 system.addForce(restraint)
-restraint.addGlobalParameter('k', 10.0*unit.kilojoules_per_mole/(unit.nanometer*unit.nanometer))
+#restraint.addGlobalParameter('k', 10.0*unit.kilojoules_per_mole/(unit.nanometer*unit.nanometer))
+restraint.addPerParticleParameter('k')
 restraint.addPerParticleParameter('x0')
 restraint.addPerParticleParameter('y0')
 restraint.addPerParticleParameter('z0')
 for atom in pdb.topology.atoms():
     if atom.name == 'CA':
-        restraint.addParticle(atom.index, pdb.positions[atom.index])
+        restraint.addParticle(atom.index, [100.0*unit.kilojoules_per_mole/(unit.nanometer*unit.nanometer), 
+              pdb.positions[atom.index][0], pdb.positions[atom.index][1], pdb.positions[atom.index][2]] )
 
 simulation = app.Simulation(pdb.topology, system, integrator, platform, platformProperties)
 simulation.context.setPositions(pdb.positions)
@@ -64,6 +66,6 @@ simulation.reporters.append(dcdReporter)
 simulation.reporters.append(dataReporter)
 simulation.step(equilibrationSteps)
 
-system.removeForce(system.getNumForces() - 1)
+#system.removeForce(system.getNumForces() - 1)
 simulation.saveState("2_equilibration.xml")
 
