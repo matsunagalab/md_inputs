869e6a53b0d434ae6430630e7ded0dd9c1e211de
ymatsunaga <ymatsunaga@mail.saitama-u.ac.jp>
2023-09-16 17:29:52 +0900

    record requency reduced in adenlate kinase

--- adk_open/3_production.py
+++ adk_open/3_production.py
@@ -29,8 +29,8 @@ platformProperties = {'Precision': 'mixed'}
 # Mac
 #platform = mm.openmm.Platform.getPlatformByName('OpenCL')
 #platformProperties = {'Precision': 'single'}
-dcdReporter = app.DCDReporter('3_production.dcd', 2500)
-dataReporter = app.StateDataReporter(sys.stdout, 2500, totalSteps=steps,
+dcdReporter = app.DCDReporter('3_production.dcd', 25000)
+dataReporter = app.StateDataReporter(sys.stdout, 25000, totalSteps=steps,
     step=True, speed=True, progress=True, potentialEnergy=True, temperature=True, separator='\t')
 #checkpointReporter = CheckpointReporter('run1_checkpoint.chk', 10000)
 
