11e86d54d19b093fa49e66b0f7de167cb24cb0f4
ymatsunaga <ymatsunaga@mail.saitama-u.ac.jp>
2023-09-30 14:07:44 +0900

    protein only build

--- proteing/2_pdb2gmx/setup.sh
+++ proteing/2_pdb2gmx/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 
-gmx pdb2gmx -v -f ../1_attach_Dye/setup.pdb -noter -ff amber03ws_dyes -water tip4p
+#gmx pdb2gmx -v -f ../1_attach_Dye/setup.pdb -noter -ff amber03ws_dyes -water tip4p
+gmx pdb2gmx -v -f ../1_attach_Dye/K10C.pdb -ignh -noter -ff amber03ws_dyes -water tip4p
 perl top2psf.pl -p topol.top -o topol.psf
 
--- proteing/6_openmm/test.py
+++ proteing/6_openmm/test.py
@@ -12,9 +12,9 @@ integrator = LangevinMiddleIntegrator(300*kelvin, 1/picosecond, 0.004*picosecond
 simulation = Simulation(top.topology, system, integrator)
 simulation.context.setPositions(gro.positions)
 simulation.minimizeEnergy()
-simulation.reporters.append(PDBReporter('output.pdb', 1000))
-simulation.reporters.append(StateDataReporter(stdout, 1000, step=True,
+simulation.reporters.append(PDBReporter('output.pdb', 1))
+simulation.reporters.append(StateDataReporter(stdout, 1, step=True,
         potentialEnergy=True, temperature=True))
-simulation.step(10000)
+simulation.step(10)
 
 
