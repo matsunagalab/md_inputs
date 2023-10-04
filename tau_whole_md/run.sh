90c58d4ec98cf10c23b821cb0f36613f18979587
ymatsunaga <ymatsunaga@mail.saitama-u.ac.jp>
2023-10-04 22:17:58 +0900

    job scripts

---
+++ tau_whole_md/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -p all
+#SBATCH -J whole
+#SBATCH -n 1  #num of total mpi processes
+#SBATCH -c 2  #num of threads per mpi processes
+#SBATCH -o run.log
+
+# set GPU ID
+#export CUDA_VISIBLE_DEVICES=0
+
+# execution
+#python3 1_build.py
+python3 2_equilibration.py
+python3 3_production.py
+
