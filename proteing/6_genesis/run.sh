33123dc1416edc5b07feae4569da4774bbdfafd5
ymatsunaga <ymatsunaga@mail.saitama-u.ac.jp>
2023-10-09 11:47:18 +0900

    updated ProteinG with GENESDIS

--- proteing/6_genesis/run.sh
+++ proteing/6_genesis/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export PATH=/opt/genesis-2.1.0/bin:$PATH
+export PATH=/opt/genesis-2.1.1/bin:$PATH
 export OMP_NUM_THREADS=1
 
-mpirun -np 8 spdyn INP1
+mpirun -np 8 spdyn INP
 
--- proteing/7_genesis/run.sh
+++ proteing/7_genesis/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export PATH=/opt/genesis-2.1.0/bin:$PATH
+export PATH=/opt/genesis-2.1.1/bin:$PATH
 export OMP_NUM_THREADS=1
 
-mpirun -np 8 spdyn INP1
+#mpirun -np 8 spdyn INP1
 mpirun -np 8 spdyn INP2
-mpirun -np 8 spdyn INP3
+#mpirun -np 8 spdyn INP3
 
