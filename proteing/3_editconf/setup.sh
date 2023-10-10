c84f14d6d8ce6e48eb02f24ba7e63d491a78a3c3
ymatsunaga <ymatsunaga@mail.saitama-u.ac.jp>
2023-10-10 11:17:24 +0900

    update box size of proteing

--- proteing/3_editconf/setup.sh
+++ proteing/3_editconf/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
 
-gmx editconf -f ../2_pdb2gmx/conf.gro -c -d 2.00 -bt cubic
+#gmx editconf -f ../2_pdb2gmx/conf.gro -c -d 2.00 -bt cubic
+gmx editconf -f ../2_pdb2gmx/conf.gro -c -box 10.50 -bt cubic
 
--- proteing/5_genion/setup.sh
+++ proteing/5_genion/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
 cp ../4_solvate/topol.top .
-gmx grompp -f ions.mdp -c ../4_solvate/out.gro -p topol.top -o ions.tpr
+gmx grompp -maxwarn 1 -f ions.mdp -c ../4_solvate/out.gro -p topol.top -o ions.tpr
 gmx genion -s ions.tpr -o ions.gro -p topol.top -pname NA -nname CL -np 8
 
